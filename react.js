de657b56565cb242997e60bc5509cfd46f38d1cd
cimfalab2 <cimfalab2@gmail.com>
2017-05-19 14:30:55 +0900

    Update react.js

--- react.js
+++ react.js
@@ -3,9 +3,7 @@ import React from 'react';
 class Hello extends React.Component {
     render() {
         return (
-            <a href="#" onClick={event => false}> {/* BAD_EVENT_HANDLER_RETURN_FALSE alarm */}
-                foo.com
-            </a>
+            <div>Hello</div>
         );
     }
 }
