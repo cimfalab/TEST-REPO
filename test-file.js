f0a481bd6232114aa6eca7c25e22972e1968e15a
cimfalab2 <cimfalab2@gmail.com>
2017-05-19 14:33:27 +0900

    Update test-file.js

--- test-file.js
+++ test-file.js
@@ -17,10 +17,10 @@
 'use strict';
 
 function foo(options) {
-    var options = options | {}; // BAD_BITWISE_OPERATOR alarm
+    var options = options || {}; // BAD_BITWISE_OPERATOR alarm
 
 
-    if (typeof a === 'Array') {
+    if (typeof a === 'string') {
         console.log('It\'s an array!');
     }
 }
